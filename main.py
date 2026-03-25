8c4821ba5c6f46142e55c79735d4f5e5a16c4a43
acrozzetti-ctrl <acrozzetti@gmail.com>
2026-03-25 18:07:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def checar():
-    print("🔥 BOT FUNCIONANDO!")
-    print("Verificando passagens... ✈️")
+import requests
 
-checar()
+ROTAS = [
+    ("GRU", "JFK", "13/12/2026"),
+    ("JFK", "MIA", "22/12/2026"),
+    ("MIA", "GRU", "10/01/2027")
+]
+
+def buscar_precos():
+    for origem, destino, data in ROTAS:
+        print(f"\n🔍 Buscando: {origem} → {destino} | {data}")
+        
+        # simulação de preço (depois a gente melhora)
+        preco = 2500
+        
+        print(f"💰 Preço encontrado: R$ {preco}")
+
+buscar_precos()
