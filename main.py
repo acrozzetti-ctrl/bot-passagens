4916a418dc0e87e945df01edb738604e7b22aaae
acrozzetti-ctrl <acrozzetti@gmail.com>
2026-03-25 18:03:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-
 def checar():
     print("🔥 BOT FUNCIONANDO!")
     print("Verificando passagens... ✈️")
 
-def run():
-    while True:
-        checar()
-        time.sleep(10)
-
-run()
+checar()
