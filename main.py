8cfd564ef6a45910eab21af9473f3e9763621459
acrozzetti-ctrl <acrozzetti@gmail.com>
2026-03-25 17:53:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-import smtplib
 import time
-from email.mime.text import MIMEText
-
-EMAIL_REMETENTE = "acrozzetti@gmail.com"
-EMAIL_SENHA = "409575"
-EMAIL_DESTINO = "acrozzetti@gmail.com"
-
-def enviar_email(msg):
-    corpo = MIMEText(msg)
-    corpo["Subject"] = "🔔 Monitor de Passagens"
-    corpo["From"] = EMAIL_REMETENTE
-    corpo["To"] = EMAIL_DESTINO
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(EMAIL_REMETENTE, EMAIL_SENHA)
-        server.send_message(corpo)
 
 def checar():
-    # SIMULA ALERTA (depois melhoramos)
-    enviar_email("Seu bot está funcionando! ✈️")
+    print("🔥 BOT FUNCIONANDO!")
+    print("Verificando passagens... ✈️")
 
 def run():
     while True:
         checar()
-        time.sleep(7200)
+        time.sleep(10)
 
 run()
