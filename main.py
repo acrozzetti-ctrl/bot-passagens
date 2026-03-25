b59d6d8ea1ab66bb4b34e9226392f2a363405b56
acrozzetti-ctrl <acrozzetti@gmail.com>
2026-03-25 18:15:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import requests
+import random
 
 ROTAS = [
-    ("GRU", "JFK", "13/12/2026"),
-    ("JFK", "MIA", "22/12/2026"),
-    ("MIA", "GRU", "10/01/2027")
+    ("GRU", "JFK", "13/12/2026", 3500),
+    ("JFK", "MIA", "22/12/2026", 800),
+    ("MIA", "GRU", "10/01/2027", 3500)
 ]
 
 def buscar_precos():
-    for origem, destino, data in ROTAS:
-        print(f"\n🔍 Buscando: {origem} → {destino} | {data}")
+    for origem, destino, data, limite in ROTAS:
+        print(f"\n🔍 {origem} → {destino} | {data}")
         
-        # simulação de preço (depois a gente melhora)
-        preco = 2500
+        # simulação de preço (vamos trocar depois por real)
+        preco = random.randint(2000, 5000)
         
-        print(f"💰 Preço encontrado: R$ {preco}")
+        print(f"💰 Preço: R$ {preco}")
+        
+        if preco < limite:
+            print("🔥 PROMOÇÃO ENCONTRADA!")
+        else:
+            print("❌ Ainda caro")
 
 buscar_precos()
