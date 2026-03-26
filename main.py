a809f35cd16b6904869d08e9e98ff47d03a1bfdb
acrozzetti-ctrl <acrozzetti@gmail.com>
2026-03-26 17:18:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def salvar_arquivo(dados, sha):
     url = f"https://api.github.com/repos/{REPO}/contents/{ARQUIVO}"
     headers = {
-        "Authorization": f"Bearer {TOKEN}",
-        "Accept": "application/vnd.github+json"
-    }
+    "Authorization": f"token {TOKEN}",
+    "Accept": "application/vnd.github+json"
+}
 
     conteudo = base64.b64encode(json.dumps(dados, indent=2).encode()).decode()
 
